71196ca5d71c65858697df151cbc0ac6195cf2c7
koten0224 <koten0224@gmail.com>
2019-11-13 01:05:21 +0800

    Update history of Leetcode until 2019-11-13

--- 0011.container-with-most-water/container-with-most-water.py
+++ 0011.container-with-most-water/container-with-most-water.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        dic={index:value for index,value in enumerate(height)}
         j=len(height)-1
         i=0
         ans=0
         while j>i:
-            a,b=dic[i],dic[j]
+            a,b=height[i],height[j]
             w=j-i
             area=w*min(a,b)
             if area>ans:
